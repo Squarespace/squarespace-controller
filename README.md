8c66a582b8ddb865a88793dc3939b00dc07af36a
George Zhang <gzhang@squarespace.com>
2017-01-19 12:25:19 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,104 @@
+Squarespace Controller
+------------------------------
+
+Controller is a lightweight control assigner for DOM elements. Controller binds functions to elements, and does the management work of making sure they are relevant, handling lifecycle after asynchronous content load.
+
+*NOTICE: This code is licensed to you pursuant to Squarespace’s Developer Terms of Use. See license section below.*
+
+## Basic Usage:
+
+A controller function is associated with a string through the method `register`. The string can then be set as the `data-controller` attribute of an element in the DOM, in order to bind the controller to the element.
+
+Inside the controller function, the DOM element is passed in as an argument.
+
+HTML
+
+```html
+<div data-controller="MyController"></div>
+```
+
+JavaScript
+
+```js
+import controller from '@squarespace/controller';
+
+function MyController(element) {
+  const handleClick = () => {
+    element.toggleClass('clicked');
+  };
+  element.addEventListener('click', handleClick);
+}
+
+controller.register('MyController', MyController);
+```
+
+## Advanced Usage
+
+### Using ES6
+
+If you prefer to handle transpiling and polyfilling on your own, you can import ES6 from Controller:
+
+```js
+import controller from '@squarespace/controller/src';
+```
+
+Alternately, Controller specifies a `module` property in `package.json` that points to the uncompiled `src/index.js`, so you may be able to simply import `@squarespace/controller` if you're using one of the following bundlers:
+* [Webpack 2](https://webpack.js.org/configuration/resolve/#resolve-mainfields)
+* [Rollup](https://github.com/rollup/rollup-plugin-node-resolve#rollup-plugin-node-resolve)
+
+
+### Ajax
+
+Controller also exports a method `refresh`. If you use Ajax loading on your site, `refresh` should be called whenever an asynchronous load occurs.
+
+Controller functions can return an object with `sync` and `destroy` methods. When `refresh` is called, the Controller manager will take stock of DOM elements, run `destroy` on any elements that no longer exist on the page, run `sync` on elements that remain on the page, and run the controller function for elements that are newly added to the page.
+
+#### Ajax Example
+
+HTML
+
+```html
+<div data-controller="MyController"></div>
+```
+
+Javascript
+
+```js
+const controller = require('@squarespace/controller');
+
+function MyController(element) {
+  const handleClick = () => {
+    element.toggleClass('clicked');
+  };
+  this.addEventListener('click', handleClick);
+  return {
+    sync: () => {
+      element.removeClass('clicked');
+    },
+    destroy: () => {
+      element.removeEventListener('click', handleClick);
+    }
+  };
+}
+
+controller.register('MyController', MyController);
+
+// Assuming this event is dispatched on ajax load
+window.addEventListener('ajax:load', controller.refresh);
+```
+
+## Reference
+
+### register(name, controller)
+Register a function to a given name string, so that setting `data-controller` attribute to that string will bind that element to the controller function.
+
+**Params**
+* name `string`
+* controller `function`
+
+### refresh()
+Refresh all controllers. To be called after the DOM has been mutated.
+
+## License
+
+Portions Copyright © 2016 Squarespace, Inc. This code is licensed to you pursuant to Squarespace’s Developer Terms of Use, available at http://developers.squarespace.com/developer-terms-of-use (the “Developer Terms”). You may only use this code on websites hosted by Squarespace, and in compliance with the Developer Terms. TO THE FULLEST EXTENT PERMITTED BY LAW, SQUARESPACE PROVIDES ITS CODE TO YOU ON AN “AS IS” BASIS WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED.
---
+++ src/index.js
@@ -0,0 +1,162 @@
+/**
+ * Cached object containing controller functions indexed by registered name string.
+ *
+ * @name CONTROLLERS_BY_NAME
+ * @type {Object}
+ */
+const CONTROLLERS_BY_NAME = {};
+
+
+/**
+ * Cached array of live controllers.
+ *
+ * @name _liveControllers
+ * @type {Array}
+ */
+let _liveControllers = [];
+
+/**
+ * Given a controller object, run its function with its element as the context
+ * and first parameter, and return whatever the result of the function call is
+ * (lifecycle methods).
+ *
+ * @method process
+ * @param {Object} controller   Controller object to process, incl. fn and element
+ * @return {*}                  Result of function call
+ */
+function process(controller) {
+  if (!controller.fn || !controller.element) {
+    return null;
+  }
+  return controller.fn(controller.element);
+}
+
+/**
+ * Compare two controller objects and returns whether they are equal.
+ *
+ * @method isEqualController
+ */
+function isEqualController(a, b) {
+  if (!a.element || !b.element || !a.fn || !b.fn) {
+    return false;
+  }
+  return a.element === b.element && a.fn === b.fn;
+}
+
+/**
+ * Registers a controller with a given name and function body.
+ *
+ * @method register
+ * @param {String} name           String to register controller fn to
+ * @param {Function} controller   Controller fn to register
+ */
+function register(name, controller) {
+  CONTROLLERS_BY_NAME[name] = controller;
+}
+
+/**
+ * Sychronize all controllers. Process new controllers, destroy controllers
+ * whose elements are no longer on the DOM, and sync others.
+ *
+ * @method refresh
+ */
+function refresh() {
+
+  // Get all nodes with controllers and convert to array
+  const nodesWithControllers = Array.from(document.querySelectorAll('[data-controller]'));
+
+  // Create array to house new controller objects
+  let newControllers = [];
+
+  // Convert found nodes to controller objects
+  // so we can do comparisons later
+  nodesWithControllers.forEach((node) => {
+
+    // Get controller names on node
+    const controllerNames = node.getAttribute('data-controller').split(',');
+
+    // Loop
+    controllerNames.forEach((controllerName) => {
+
+      controllerName = controllerName.trim();
+
+      const controller = CONTROLLERS_BY_NAME[controllerName];
+
+      if (!controller) {
+        return;
+      }
+
+      // Add to new controller array
+      newControllers.push({
+        namespace: controllerName,
+        element: node,
+        fn: controller
+      });
+
+    });
+
+  });
+
+  // Loop through live controllers and find ones that need to be destroyed
+  // or synced
+  _liveControllers = _liveControllers.filter((liveController) => {
+
+    // Boolean to indicate whether one of new controllers matches current
+    // live controller.
+    const isControllerActive = newControllers.some((newController) => {
+      return isEqualController(liveController, newController);
+    });
+
+    if (isControllerActive) {
+
+      // Controller element is still in the DOM, run sync method of controller.
+      if (liveController.methods && liveController.methods.sync) {
+        liveController.methods.sync.apply(liveController.element, null);
+      }
+
+      // Remove existing controllers from new controllers array, so they are not
+      // reinitialized.
+      newControllers = newControllers.filter((newController) => {
+        return !isEqualController(liveController, newController);
+      });
+
+    } else {
+
+      // Controller element is no longer in the DOM, call destructor method of
+      // controller.
+      if (liveController.methods && liveController.methods.destroy) {
+        liveController.methods.destroy.apply(liveController.element, null);
+      }
+
+    }
+    return isControllerActive;
+  });
+
+
+  // Process new controllers for the first time
+  newControllers.forEach((controller) => {
+
+    controller.methods = process(controller);
+    _liveControllers.push(controller);
+
+    // Add controller bound info
+    let boundControllersName = [];
+    if (controller.element.hasAttribute('data-controllers-bound')) {
+      const existingControllers = controller.element.getAttribute('data-controllers-bound').split(', ');
+      boundControllersName = boundControllersName.concat(existingControllers);
+    }
+
+    boundControllersName.push(controller.namespace);
+
+    controller.element.setAttribute('data-controllers-bound', boundControllersName.join(', '));
+
+  });
+
+}
+
+document.addEventListener('DOMContentLoaded', refresh);
+
+export default {
+  refresh,
+  register
+};
